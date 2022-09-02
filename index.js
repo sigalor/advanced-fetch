463573e9effc83aa531e6f2f0b5bca760ad76e26
sigalor <nalenz@protonmail.com>
2022-09-02 11:23:46 +0200

    Add .prettierrc.json, reformat source

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
-const util = require("util");
-const fs = require("fs-extra");
-const nodeFetch = require("node-fetch");
-const { CookieJar } = require("tough-cookie");
-const fetchCookie = require("fetch-cookie");
-const FormData = require("form-data");
-const { URLSearchParams } = require("url");
-const querystring = require("querystring");
-const iconv = require("iconv-lite");
-const https = require("https");
+const util = require('util');
+const fs = require('fs-extra');
+const nodeFetch = require('node-fetch');
+const { CookieJar } = require('tough-cookie');
+const fetchCookie = require('fetch-cookie');
+const FormData = require('form-data');
+const { URLSearchParams } = require('url');
+const querystring = require('querystring');
+const iconv = require('iconv-lite');
+const https = require('https');
 
 CookieJar.deserialize = util.promisify(CookieJar.deserialize);
 
@@ -18,12 +18,7 @@ function promisifyCookieJar(jar) {
 }
 
 class Fetch {
-  constructor({
-    cookiesFilename,
-    encoding,
-    commonFetchParams,
-    ignoreInvalidHttps,
-  } = {}) {
+  constructor({ cookiesFilename, encoding, commonFetchParams, ignoreInvalidHttps } = {}) {
     if (ignoreInvalidHttps) {
       commonFetchParams = {
         ...commonFetchParams,
@@ -37,9 +32,7 @@ class Fetch {
 
     return (async () => {
       if (this.cookiesFilename && (await fs.exists(this.cookiesFilename)))
-        this.jar = promisifyCookieJar(
-          await CookieJar.deserialize(await fs.readJSON(this.cookiesFilename))
-        );
+        this.jar = promisifyCookieJar(await CookieJar.deserialize(await fs.readJSON(this.cookiesFilename)));
       else this.jar = promisifyCookieJar(new CookieJar());
       this.fetch = fetchCookie(nodeFetch, this.jar);
       return this;
@@ -48,10 +41,9 @@ class Fetch {
 
   async requestWithHeaders(url, params = {}) {
     // process GET query string
-    let queryStr = "";
+    let queryStr = '';
     if (params.query) {
-      if (Object.keys(params.query).length > 0)
-        queryStr = "&" + querystring.stringify(params.query);
+      if (Object.keys(params.query).length > 0) queryStr = '&' + querystring.stringify(params.query);
       delete params.query;
     }
 
@@ -65,20 +57,20 @@ class Fetch {
     } else if (params.formData) {
       const form = new FormData();
       const appendToForm = (k, v) => {
-        if (Array.isArray(v)) v.forEach((x) => form.append(k, x.toString()));
-        if (typeof v === "object") form.append(k, v.value, v.options);
+        if (Array.isArray(v)) v.forEach(x => form.append(k, x.toString()));
+        if (typeof v === 'object') form.append(k, v.value, v.options);
         else form.append(k, v.toString());
       };
 
       for (const [k, v] of Object.entries(params.formData)) {
-        if (Array.isArray(v)) v.forEach((vItem) => appendToForm(k, vItem));
+        if (Array.isArray(v)) v.forEach(vItem => appendToForm(k, vItem));
         else appendToForm(k, v);
       }
       params.body = form;
       delete params.formData;
     } else if (params.json) {
       params.body = JSON.stringify(params.json);
-      params.headers["Content-Type"] = "application/json";
+      params.headers['Content-Type'] = 'application/json';
       delete params.json;
     }
 
@@ -87,17 +79,13 @@ class Fetch {
       ...params,
       ...this.commonFetchParams,
     });
-    if (resp.status >= 500)
-      throw new Error(resp.status + " " + resp.statusText);
+    if (resp.status >= 500) throw new Error(resp.status + ' ' + resp.statusText);
     await this.storeCookies();
 
     // if an encoding was given in the Fetch constructur, the response still needs to be converted from that to UTF-8
     let encoding = params.encoding || this.encoding;
-    let content = await (params.returnBuffer || encoding
-      ? resp.buffer()
-      : resp.text());
-    if (!params.returnBuffer && encoding)
-      content = iconv.decode(content, encoding);
+    let content = await (params.returnBuffer || encoding ? resp.buffer() : resp.text());
+    if (!params.returnBuffer && encoding) content = iconv.decode(content, encoding);
 
     return {
       status: resp.status,
@@ -108,23 +96,22 @@ class Fetch {
 
   async requestWithFullResponse(url, params = {}) {
     // only let node-fetch follow redirects if that's explicitly stated
-    if (params.redirect === "follow")
-      return (await this.requestWithHeaders(url, params)).content;
+    if (params.redirect === 'follow') return (await this.requestWithHeaders(url, params)).content;
 
     // otherwise follow them manually, because otherwise Set-Cookie is ignored for redirecting sites
     let nextUrl = url;
     let currResp;
-    params = { ...params, redirect: "manual" };
+    params = { ...params, redirect: 'manual' };
     while (true) {
       currResp = await this.requestWithHeaders(nextUrl, params);
       if (currResp.status < 300 || currResp.status >= 400) break;
       else if (currResp.status !== 301 && currResp.status !== 302)
-        throw new Error("unknown HTTP redirect status: " + currResp.status);
+        throw new Error('unknown HTTP redirect status: ' + currResp.status);
 
       const loc = currResp.headers.location;
       if (!loc || (Array.isArray(loc) && loc.length !== 1)) break;
       nextUrl = Array.isArray(loc) ? loc[0] : loc;
-      params = { method: "GET" };
+      params = { method: 'GET' };
     }
 
     return currResp;
@@ -135,19 +122,19 @@ class Fetch {
   }
 
   get(url, params = {}) {
-    return this.request(url, { ...params, method: "GET" });
+    return this.request(url, { ...params, method: 'GET' });
   }
 
   post(url, params = {}) {
-    return this.request(url, { ...params, method: "POST" });
+    return this.request(url, { ...params, method: 'POST' });
   }
 
   put(url, params = {}) {
-    return this.request(url, { ...params, method: "PUT" });
+    return this.request(url, { ...params, method: 'PUT' });
   }
 
   delete(url, params = {}) {
-    return this.request(url, { ...params, method: "DELETE" });
+    return this.request(url, { ...params, method: 'DELETE' });
   }
 
   async storeCookies() {
@@ -158,9 +145,7 @@ class Fetch {
   }
 
   async getCookie(key) {
-    const cookie = (await this.jar.serialize()).cookies.find(
-      (c) => c.key === key
-    );
+    const cookie = (await this.jar.serialize()).cookies.find(c => c.key === key);
     if (!cookie) return;
     return cookie.value;
   }
