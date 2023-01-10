60c020b5a9dcf280da6b8e4e57a96630abd19bf9
sigalor <nalenz@protonmail.com>
2023-01-10 11:15:35 +0100

    test: add several new unit tests for JSON parsing and cookie handling

--- test/index.test.ts
+++ test/index.test.ts
@@ -17,19 +17,46 @@ afterAll(async () => {
 });
 
 describe('advanced-fetch', () => {
-  test('can GET /', async () => {
+  test('can get a simple string', async () => {
     const fetch = new Fetch();
     const response = await fetch.get(`${baseUrl}/`);
     expect(response).toBe('Hello World!');
   });
 
-  test('returns the given URL when using manual redirects', async () => {
+  test('can get JSON and automatically parses it', async () => {
+    const fetch = new Fetch();
+    const response = await fetch.get(`${baseUrl}/json`);
+    expect(response.message).toEqual('Hello World!');
+  });
+
+  test("doesn't parse JSON when told not to", async () => {
+    const fetch = new Fetch();
+    const response = await fetch.get(`${baseUrl}/json`, { returnType: 'string' });
+    expect(response).toEqual('{"message":"Hello World!"}');
+  });
+
+  test('returns the URL chain when using manual redirects', async () => {
     const fetch = new Fetch();
     const response = await fetch.requestWithFullResponse(`${baseUrl}/redirect`);
 
     expect(response.urls).toHaveLength(2);
     expect(response.urls![0]).toBe(`${baseUrl}/redirect`);
     expect(response.urls![1]).toBe(`${baseUrl}/`);
+  });
+
+  test('can retain a cookie', async () => {
+    const fetch = new Fetch();
+    await fetch.post(`${baseUrl}/set-cookies`, { json: { mycookie: 'secret' } });
+    const response = await fetch.get(`${baseUrl}/get-cookies`);
+    expect(response.mycookie).toBe('secret');
+  });
+
+  test('can retain a cookie across a redirect', async () => {
+    const fetch = new Fetch();
+    const response = await fetch.post(`${baseUrl}/set-cookies-and-redirect-to-get-cookies`, {
+      json: { mycookie: 'secret' },
+    });
+    expect(response.mycookie).toBe('secret');
   });
 
   // TODO: add many more unit tests for cookies, encoding, etc.
--- test/server.ts
+++ test/server.ts
@@ -1,15 +1,42 @@
 import express from 'express';
+import cookieParser from 'cookie-parser';
+import bodyParser from 'body-parser';
 import http from 'http';
 
 const app = express();
+app.use(cookieParser());
+app.use(bodyParser.json());
+
 let server: http.Server | undefined = undefined;
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
+app.get('/json', (req, res) => {
+  res.json({ message: 'Hello World!' });
+});
+
 app.get('/redirect', (req, res) => {
   res.redirect(301, '/');
+});
+
+app.post('/set-cookies', (req, res) => {
+  for (const [k, v] of Object.entries(req.body)) {
+    res.cookie(k, v);
+  }
+  res.status(204).send();
+});
+
+app.post('/set-cookies-and-redirect-to-get-cookies', (req, res) => {
+  for (const [k, v] of Object.entries(req.body)) {
+    res.cookie(k, v);
+  }
+  res.redirect(301, '/get-cookies');
+});
+
+app.get('/get-cookies', (req, res) => {
+  res.json(req.cookies);
 });
 
 export async function startServer(port: number): Promise<void> {
